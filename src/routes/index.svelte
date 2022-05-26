4a577ef5f9693eddbe5e147064e81f9fd64a6e2e
Hisam Fahri <me@hisamafahri.com>
2022-05-26 12:50:06 +0700

    feat(editor): change file name

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,6 +9,8 @@
     Tabs,
     Tab,
     TabContent,
+    Modal,
+    TextInput,
   } from "carbon-components-svelte";
   import { ArrowRight } from "carbon-icons-svelte";
   import type monaco from "monaco-editor";
@@ -56,13 +58,20 @@
       editor.dispose();
     };
   });
+  let open = false;
+  let defaultFileName = "Untitled";
+  let fileName = defaultFileName;
 </script>
 
 <Content>
   <Grid>
     <Row>
       <Column>
-        <h1 style="margin-bottom: 24px;">Let Your Codes Fly 🚀</h1>
+        <h1 style="margin-bottom: 16px;">Let Your Codes Fly 🚀</h1>
+        <p style="margin-bottom: 36px;">
+          Share your codes with your friends, collagues, or anyone easily, for
+          free!
+        </p>
         <TextArea
           labelText="Code Desription"
           placeholder="Enter your code description..."
@@ -76,7 +85,9 @@
       </Column>
       <Column>
         <Tabs type="container" style="padding: 0;">
-          <Tab label="Untitled" />
+        <div on:dblclick={() => (open = true)}>
+            <Tab label={fileName} />
+          </div>
           <svelte:fragment slot="content">
             <TabContent style="padding: 0px; height: 80vh; overflow: hidden">
               <div bind:this={divEl} style="height: 100%;" />
@@ -87,3 +98,22 @@
     </Row>
   </Grid>
 </Content>
+
+<!-- Change file name modal -->
+<Modal
+  bind:open
+  modalHeading="Change File Name"
+  primaryButtonText="Save"
+  secondaryButtonText="Cancel"
+  on:click:button--secondary={() => (open = false)}
+  on:click:button--primary={() => (open = false, fileName = defaultFileName)}
+  on:open
+  on:close
+  on:submit
+>
+  <TextInput
+    labelText="File Name"
+    placeholder="Enter file name..."
+    bind:value={defaultFileName}
+  />
+</Modal>
